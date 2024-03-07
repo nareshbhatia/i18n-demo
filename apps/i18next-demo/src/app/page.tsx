6c322e333ff9f46861ce2d06198de1376294c70f
Naresh Bhatia <me@nareshbhatia.dev>
2024-03-07 02:09:53 -0500

    feat: add ride request form

--- apps/i18next-demo/src/app/page.tsx
+++ apps/i18next-demo/src/app/page.tsx
@@ -1,10 +1,39 @@
 import { AppHeader } from '@/components/AppHeader';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import * as React from 'react';
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-xl">
+    <div className="mx-auto flex max-w-xl flex-col gap-10 px-4">
       <AppHeader />
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Where to, Naresh?</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-6">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="pickup-location">Pickup spot</Label>
+                <Input id="pickup-location" />
+              </div>
+
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="dropoff-location">Your destination</Label>
+                <Input id="dropoff-location" />
+              </div>
+
+              <Button aria-label="Request Ride" type="submit">
+                REQUEST RIDE
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- apps/i18next-demo/src/components/AppHeader/AppHeader.tsx
+++ apps/i18next-demo/src/components/AppHeader/AppHeader.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from './ModeToggle';
 
 export function AppHeader() {
   return (
-    <div className="flex h-16 items-center px-4">
+    <div className="flex h-16 items-center">
       <h1 className="flex-1 text-3xl font-semibold">driverless</h1>
       <ModeToggle />
     </div>
